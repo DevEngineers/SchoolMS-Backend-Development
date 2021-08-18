ed5e92c1e9f3c5abc493b6e7bfa1ab27cbc69625
Mohamed Akeel <akeel1447@gmail.com>
2021-08-18 23:42:48 +0530

    Exam timetable route implementation is complete

--- routes/examTimetablesRouter.js
+++ routes/examTimetablesRouter.js
@@ -1,5 +1,80 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const ExamTimetable = require('../models/ExamTimetable');
+
 const examTimetableRouter = express.Router();
 
+examTimetableRouter.use(bodyParser.json());
+
+
+examTimetableRouter.route('/')
+    .get((req,res,next) =>{
+        ExamTimetable.find({})
+            .then((examTimetables) =>{
+                res.statusCode = 200;
+                res.setHeader('Content-Type')
+                res.json(examTimetables);
+            },(err) =>{
+                next(err);
+            })
+            .catch((err) =>{
+                next(err);
+            })
+
+    })
+    .get((req,res,next) => {
+        ExamTimetable.findById(req.params.eTimetableID)
+            .then((examTimetable) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(examTimetable);
+            },(err) => {
+                next(err);
+            })
+            .catch((err) => {
+                next(err);
+            })
+    })
+    .post((req,res,next) =>{
+        ExamTimetable.create(req.body)
+            .then((examTimetable) =>{
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(examTimetable);
+            },(err) =>{
+                next(err);
+            })
+            .catch((err) =>{
+                next(err);
+            })
+    })
+    .put((req, res, next) => {
+        ExamTimetable.findByIdAndUpdate(req.params.eTimetableID,{
+            $set:req.body
+        },{ new :true })
+            .then((examTimetable) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(examTimetable);
+            },(err) => {
+                next(err);
+            })
+            .catch((err) => {
+                next(err);
+            })
+    })
+    .delete((req, res, next) => {
+        ExamTimetable.findByIdAndRemove(req.params.eTimetableID)
+            .then((examTimetable) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(examTimetable);
+            },(err) => {
+                next(err);
+            })
+            .catch((err) => {
+                next(err);
+            })
+    });
 
 module.exports = examTimetableRouter;
