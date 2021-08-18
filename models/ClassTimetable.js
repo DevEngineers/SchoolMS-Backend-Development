ce6902e4c9f4b0c882da14f30731285f54e63715
Mohamed Akeel <akeel1447@gmail.com>
2021-08-18 23:08:14 +0530

    ClassTimetable module Created and implementation complete

---
+++ models/ClassTimetable.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const classTimetableSchema = new Schema({
+    class:{
+        type:String,
+        required:true,
+        default:''
+    },
+    classType:{
+        type:String,
+        required:true,
+        default:''
+    },
+    year:{
+        type:String,
+        required:true,
+        default:''
+    },
+    startSlot:{
+        type:[String],
+        required:true,
+        default:['']
+    },
+    endSlot:{
+        type:[String],
+        required:true,
+        default:['']
+    },
+    monday:{
+        type:[String],
+        required:true,
+        default:['']
+    },
+    tuesday:{
+        type:[String],
+        required:true,
+        default:['']
+    },
+    wednesday:{
+        type:[String],
+        required:true,
+        default:['']
+    },
+    thursday:{
+        type:[String],
+        required:true,
+        default:['']
+    },
+    friday:{
+        type:[String],
+        required:true,
+        default:['']
+    }
+},{
+    timestamps:true
+})
+
+let ClassTimetable = mongoose.model('ClassTimetable',classTimetableSchema);
+
+module.exports = ClassTimetable;
