5ed578be855af975deab40b0cd34c2b32164249b
Mohamed Akeel <akeel1447@gmail.com>
2021-08-18 23:35:56 +0530

    Class timetable route implementation is complete

--- routes/classTimetablesRouter.js
+++ routes/classTimetablesRouter.js
@@ -1,5 +1,80 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const ClassTimetable = require('../models/ClassTimetable');
+
 const classTimetableRouter = express.Router();
 
+classTimetableRouter.use(bodyParser.json());
+
+
+classTimetableRouter.route('/')
+    .get((req,res,next) =>{
+        ClassTimetable.find({})
+            .then((classTimetables) =>{
+                res.statusCode = 200;
+                res.setHeader('Content-Type')
+                res.json(classTimetables);
+            },(err) =>{
+                next(err);
+            })
+            .catch((err) =>{
+                next(err);
+            })
+
+    })
+    .get((req,res,next) => {
+        ClassTimetable.findById(req.params.cTimetableID)
+            .then((classTimetable) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(classTimetable);
+            },(err) => {
+                next(err);
+            })
+            .catch((err) => {
+                next(err);
+            })
+    })
+    .post((req,res,next) =>{
+        ClassTimetable.create(req.body)
+            .then((classTimetable) =>{
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(classTimetable);
+            },(err) =>{
+                next(err);
+            })
+            .catch((err) =>{
+                next(err);
+            })
+    })
+    .put((req, res, next) => {
+        ClassTimetable.findByIdAndUpdate(req.params.cTimetableID,{
+            $set:req.body
+        },{ new :true })
+            .then((classTimetable) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(classTimetable);
+            },(err) => {
+                next(err);
+            })
+            .catch((err) => {
+                next(err);
+            })
+    })
+    .delete((req, res, next) => {
+        ClassTimetable.findByIdAndRemove(req.params.cTimetableID)
+            .then((classTimetable) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(classTimetable);
+            },(err) => {
+                next(err);
+            })
+            .catch((err) => {
+                next(err);
+            })
+    });
 
 module.exports = classTimetableRouter;
