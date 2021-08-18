5790852f1f4e2f200608ba12e4ef000b91f31bed
Mohamed Akeel <akeel1447@gmail.com>
2021-08-18 23:08:37 +0530

    ExamTimetable module Created and implementation complete

---
+++ models/ExamTimetable.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const examTimetableSchema = new Schema({
+    class:{
+        type:String,
+        required:true,
+        default:''
+    },
+    classType:{
+        type:String,
+        required:true,
+        default:''
+    },
+    year:{
+        type:String,
+        required:true,
+        default:''
+    },
+    term:{
+        type:String,
+        required:true,
+        default:''
+    },
+    startSlot:{
+        type:[String],
+        required:true,
+        default:['']
+    },
+    endSlot:{
+        type:[String],
+        required:true,
+        default:['']
+    },
+    examDates:{
+        type:[String],
+        required:true,
+        default:['']
+    },
+    examSubjects:{
+        type:[String],
+        required:true,
+        default:['']
+    }
+},{
+    timestamps:true
+})
+
+let ExamTimetable = mongoose.model('ExamTimetable',examTimetableSchema);
+
+module.exports = ExamTimetable;
