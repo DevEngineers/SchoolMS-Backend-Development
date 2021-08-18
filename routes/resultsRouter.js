f67a0647219125f0ec016a63e05926e659a51174
Mohamed Akeel <akeel1447@gmail.com>
2021-08-18 23:50:23 +0530

    Result route implementation is complete

--- routes/resultsRouter.js
+++ routes/resultsRouter.js
@@ -1,5 +1,82 @@
 const express = require('express');
+const bodyParser = require("body-parser");
+const Result = require("../models/Result");
+
+
 const resultsRouter = express.Router();
+
+resultsRouter.use(bodyParser.json());
+
+
+resultsRouter.route('/')
+    .get((req,res,next) =>{
+        Result.find({})
+            .then((results) =>{
+                res.statusCode = 200;
+                res.setHeader('Content-Type')
+                res.json(results);
+            },(err) =>{
+                next(err);
+            })
+            .catch((err) =>{
+                next(err);
+            })
+
+    })
+    .get((req,res,next) => {
+        Result.findById(req.params.resultID)
+            .then((result) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(result);
+            },(err) => {
+                next(err);
+            })
+            .catch((err) => {
+                next(err);
+            })
+    })
+    .post((req,res,next) =>{
+        Result.create(req.body)
+            .then((result) =>{
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(result);
+            },(err) =>{
+                next(err);
+            })
+            .catch((err) =>{
+                next(err);
+            })
+    })
+    .put((req, res, next) => {
+        Result.findByIdAndUpdate(req.params.eTimetableID,{
+            $set:req.body
+        },{ new :true })
+            .then((result) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(result);
+            },(err) => {
+                next(err);
+            })
+            .catch((err) => {
+                next(err);
+            })
+    })
+    .delete((req, res, next) => {
+        Result.findByIdAndRemove(req.params.eTimetableID)
+            .then((result) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(result);
+            },(err) => {
+                next(err);
+            })
+            .catch((err) => {
+                next(err);
+            })
+    });
 
 
 module.exports = resultsRouter;
