d51c6e20e0180bafbd05082c5333a1cbfd2b0261
Zumry <amzumry@gmail.com>
2021-08-21 00:06:38 +0530

    subject part done.

--- models/Subject.js
+++ models/Subject.js
@@ -2,10 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const subjectSchema = new Schema({
-    class:{
+    subject:{
         type:String,
         required:true,
         default:''
+    },
+    class:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:'classes'
+    },
+    teacher:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:'teachers'
     },
 },{
     timestamps:true
--- routes/subjectsRouter.js
+++ routes/subjectsRouter.js
@@ -1,5 +1,81 @@
 const express = require('express');
+const bodyParser = require("body-parser");
+const Subject = require("../models/Subject");
+
 const subjectsRouter = express.Router();
 
+subjectsRouter.use(bodyParser.json());
+
+subjectsRouter.route('/')
+    .get((req,res,next) =>{
+        Subject.find({})
+            .then((subject) =>{
+                res.statusCode = 200;
+                res.setHeader('Content-Type','application/json')
+                res.json(subject);
+            },(err) =>{
+                next(err);
+            })
+            .catch((err) =>{
+                next(err);
+            })
+
+    })
+    .post((req,res,next) =>{
+        Subject.create(req.body)
+            .then((subject) =>{
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(subject);
+            },(err) =>{
+                next(err);
+            })
+            .catch((err) =>{
+                next(err);
+            })
+    });
+
+subjectsRouter.route('/:id')
+    .get((req,res,next) => {
+        Subject.findById(req.params.id)
+            .then((subject) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(subject);
+            },(err) => {
+                next(err);
+            })
+            .catch((err) => {
+                next(err);
+            })
+    })
+    .put((req, res, next) => {
+        Subject.findByIdAndUpdate(req.params.id,{
+            $set:req.body
+        },{ new :true })
+            .then((subject) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(subject);
+            },(err) => {
+                next(err);
+            })
+            .catch((err) => {
+                next(err);
+            })
+    })
+    .delete((req, res, next) => {
+        Subject.findByIdAndRemove(req.params.id)
+            .then((subject) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(subject);
+            },(err) => {
+                next(err);
+            })
+            .catch((err) => {
+                next(err);
+            })
+    });
 
 module.exports = subjectsRouter;
