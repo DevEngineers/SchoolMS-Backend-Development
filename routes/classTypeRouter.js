60339bd77de30774c9dc8e9c05cfa7a50ac32ef1
Mohamed Akeel <akeel1447@gmail.com>
2021-08-22 16:37:34 +0530

    new router created for classType

---
+++ routes/classTypeRouter.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const ClassType = require("../models/ClassType");
+
+const classTypeRouter = express.Router();
+
+classTypeRouter.use(bodyParser.json());
+
+classTypeRouter.route('/')
+    .get(async (req,res,next) =>{
+        await ClassType.find({})
+            .then((ClassType) =>{
+                res.statusCode = 200;
+                res.setHeader('Content-Type','application/json')
+                res.json(ClassType);
+            },(err) =>{
+                next(err);
+            })
+            .catch((err) =>{
+                next(err);
+            })
+
+    })
+    .post(async (req,res,next) =>{
+        console.log(req.body)
+        await ClassType.create(req.body)
+            .then((Class) =>{
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(Class);
+            },(err) =>{
+                next(err);
+            })
+            .catch((err) =>{
+                next(err);
+            })
+    });
+
+module.exports = classTypeRouter;
