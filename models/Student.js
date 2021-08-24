b77f9c7ac94a0999d013ead4af68e68829353540
Zumry <amzumry@gmail.com>
2021-08-24 20:38:42 +0530

    initial commit

---
+++ models/Student.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const studentSchema = new Schema({
+    studentName:{
+        type:String,
+        required:true,
+        default:''
+    },
+    guardian:{
+        type:String,
+        required:true,
+        default:''
+    },
+    phone:{
+        type:String,
+        required:true,
+        default:''
+    },
+    dob:{
+        type:String,
+        required:true,
+        default:''
+    },
+    address:{
+        type:String,
+        required:true,
+        default:''
+    },
+    schoolBranch:{
+        type:String,
+        required:true,
+        default:''
+    },
+    class:{
+        type:String,
+        required:true,
+        default:''
+    },
+    classType:{
+        type:String,
+        required:true,
+        default:''
+    },
+    gender:{
+        type:String,
+        required:true,
+        default:''
+    },
+},{
+    timestamps:true
+})
+
+let Student = mongoose.model('Student',studentSchema);
+
+module.exports = Student;
