8412193f7bed92046a60c2917cd3733e141b372c
Mohamed Akeel <akeel1447@gmail.com>
2021-09-05 17:14:38 +0530

    final commit

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -10,6 +10,7 @@ usersRouter.use(bodyParser.json());
 usersRouter.route('/')
     .get(async (req,res,next) =>{
       await User.find({})
+          .populate('branch')
           .then((users) =>{
             res.statusCode = 200;
             res.setHeader('Content-Type','application/json')
@@ -39,6 +40,7 @@ usersRouter.route('/')
 usersRouter.route('/:id')
     .get(async (req,res,next) => {
         await User.findById(req.params.id)
+            .populate('branch')
             .then((user) => {
                 res.statusCode = 200;
                 res.setHeader('Content-Type', 'application/json');
@@ -51,19 +53,36 @@ usersRouter.route('/:id')
             })
     })
     .put(async (req, res, next) => {
-      await User.findByIdAndUpdate(req.params.id,{
-        $set:req.body
-      },{ new :true })
-          .then((user) => {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.json(user);
-          },(err) => {
-            next(err);
-          })
-          .catch((err) => {
-            next(err);
-          })
+        await User.findById(req.params.id)
+            .then((user) => {
+
+                let newUser = req.body;
+                if(newUser.password !== ''){
+                    user.password = newUser.password
+                }else {
+                    user.username = newUser.username;
+                    user.email = newUser.email;
+                }
+
+                User.findByIdAndUpdate(req.params.id,{
+                    $set:user
+                },{ new :true })
+                    .then((user) => {
+                        res.statusCode = 200;
+                        res.setHeader('Content-Type', 'application/json');
+                        res.json(user);
+                    },(err) => {
+                        next(err);
+                    })
+                    .catch((err) => {
+                        next(err);
+                    })
+            },(err) => {
+                next(err);
+            })
+            .catch((err) => {
+                next(err);
+        })
     })
     .delete(async (req, res, next) => {
       await User.findByIdAndRemove(req.params.id)
